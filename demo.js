c9c09d9c0a41977bc193ad97d520191dc6e96441
TARUN SAINI <tarunsaini@TARUNs-MacBook-Air.local>
2025-05-17 22:25:02 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,37 @@
+// let score="33anc"
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInnumber=Number(score)
+// console.log(typeof (valueInnumber))
+// console.log(valueInnumber)
+
+
+
+// let hola=null
+// console.log(typeof hola)
+// console.log(typeof (hola))
+
+// let vin=Number(hola)
+// console.log(typeof (vin))
+// console.log(vin)
+
+
+
+// let he=undefined
+// console.log(typeof he)
+// console.log(typeof (he))
+
+// let vinu=Number(he)
+// console.log(typeof (vinu))
+// console.log(vinu)
+
+//"33"->33
+//"33abc"->nan(type of nan is also a number)
+//true->1,false->0
+let isLoggedin=""
+let cb=Boolean(isLoggedin)
+console.log(cb)
+//1->true,0->false
+//" "->false ,"gfxhj"->true
+
